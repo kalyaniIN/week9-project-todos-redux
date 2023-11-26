07995bffdb3401d497a6751326b7ff1075ebef00
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-11-26 07:19:40 +0100

    Disabled clear button when there is no items in the todo list

--- src/components/ClearTodo.jsx
+++ src/components/ClearTodo.jsx
@@ -1,16 +1,21 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { openModal } from "../reducers/modal/modalSlice";
 
 export const ClearTodo = () => {
   const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todo.todoListItems);
 
   const handleClearTodo = () => {
     dispatch(openModal());
   };
 
   return (
-    <button className="btn clearBtn" onClick={handleClearTodo}>
+    <button
+      className="btn clearBtn"
+      disabled={todos.length <= 0}
+      onClick={handleClearTodo}
+    >
       Clear Todos
     </button>
   );
